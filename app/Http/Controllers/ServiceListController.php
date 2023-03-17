9c8f4d479cd39bfcf448dc5f117406f7066be9c0
Jude Kikuyu <jkikuyu@gmail.com>
2023-03-17 13:21:02 +0300

    include redirect in servicelist controller

--- app/Http/Controllers/ServiceListController.php
+++ app/Http/Controllers/ServiceListController.php
@@ -11,12 +11,20 @@ class ServiceListController extends Controller
     public function store()
     {
         RepairService::create($this->validateRequest());
+        return redirect('/servicelist');
     }
+
     public function update(RepairService $repairService)
     {
         $data = $this->validateRequest();
 
         $repairService->update($this->validateRequest());
+        return redirect('/servicelist');
+    }
+    public function destroy(RepairService $repairService)
+    {
+        $repairService->delete();
+        return redirect('/servicelist');
     }
     protected function validateRequest()
     {
